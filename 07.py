ac1b6dd0f9287414db33f29cde3671f758b6aad4
Ruslaner1 <ruslan23981@gmail.com>
2024-11-07 15:01:52 +0500

    07 сделан

---
+++ 07.py
@@ -0,0 +1,6 @@
+print("Введите три слова: название фильма, название кинотеатра, время")
+phrase1 = input()
+phrase2 = input()
+phrase3 = input()
+
+print('Билет на ',phrase1, 'в' ,phrase2, 'на' ,phrase3)
